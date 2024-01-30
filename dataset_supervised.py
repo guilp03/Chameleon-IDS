734d6a2c8ae3a6fac1736a0c1c43b16945f5053f
guilp03 <108680176+guilp03@users.noreply.github.com>
2024-01-30 16:30:06 -0300

    quase terminando o pso

--- dataset_supervised.py
+++ dataset_supervised.py
@@ -36,43 +36,52 @@ initial_len = df.shape[0]
 df = df.dropna()
 print(f'Tamanho inicial: {initial_len}, tamanho final {df.shape[0]} | Descartados {initial_len - df.shape[0]} registros com valores NA')
 #DIVISÃO DO CONJUNTO DE TREINO, VALIDAÇÃO E TESTE
-df_train = df.sample(frac = 0.6, random_state = 33)
-df_val_test = df.drop(df_train.index)
+columnsName = df.drop(labels= 'class', axis= 1).columns.values.tolist()
 
-df_train = df_train.reset_index(drop=True)
-df_val_test = df_val_test.reset_index(drop=True)
+#FUNÇÃO QUE CALCULA O F1 DO RANDOM FOREST DADO UM CONJUNTO DE FEATURES
+def accuracy_calc(x):
+  x_train, y_train, x_val, y_val = conjuntos(df[x])
+  rf_model = rf.RandomForest(42, x_train, y_train)
+  f1_rf = rf.get_metrics(rf_model, x_val, y_val)
+  print('Accuracy:',f1_rf)
+  return f1_rf
 
-x_train = df_train.drop('class', axis='columns')
-classes_train = df_train['class']
-y_train = classes_train.apply(lambda c: 0 if c == 'normal' else 1)
-#print(y_train)
-x_val, x_test, classes_val, classes_test = train_test_split(df_val_test.drop('class', axis='columns'), df_val_test['class'], test_size=0.65, stratify=df_val_test['class'], random_state=33)
-x_val, x_test = x_val.reset_index(drop=True), x_test.reset_index(drop=True)
-classes_val, classes_test =  classes_val.reset_index(drop=True), classes_test.reset_index(drop=True)
+def conjuntos(df): 
+  df_train = df.sample(frac = 0.6, random_state = 33)
+  df_val_test = df.drop(df_train.index)
 
-y_val, y_test = classes_val.apply(lambda c: 0 if c == 'normal' else 1), classes_test.apply(lambda c: 0 if c == 'normal' else 1)
+  df_train = df_train.reset_index(drop=True)
+  df_val_test = df_val_test.reset_index(drop=True)
 
-del df_train, df_val_test  
-#print(x_train)
-#NORMALIZANDO DADOS
-#train
-std_scaler = StandardScaler()
-colunas_numericas = x_train.select_dtypes(include=['number'])
-colunas_numericas_scaler = pd.DataFrame(std_scaler.fit_transform(colunas_numericas), columns=colunas_numericas.columns)
-x_train = colunas_numericas_scaler
-#print(x_train)
-#val
-colunas_numericas = x_val.select_dtypes(include=['number'])
-colunas_numericas_scaler = pd.DataFrame(std_scaler.fit_transform(colunas_numericas), columns=colunas_numericas.columns)
-x_val = colunas_numericas_scaler
-#test
-colunas_numericas = x_test.select_dtypes(include=['number'])
-colunas_numericas_scaler = pd.DataFrame(std_scaler.fit_transform(colunas_numericas), columns=colunas_numericas.columns)
-x_test = colunas_numericas_scaler
+  x_train = df_train.drop('class', axis='columns')
+  classes_train = df_train['class']
+  y_train = classes_train.apply(lambda c: 0 if c == 'normal' else 1)
+  #print(y_train)
+  x_val, x_test, classes_val, classes_test = train_test_split(df_val_test.drop('class', axis='columns'), df_val_test['class'], test_size=0.65, stratify=df_val_test['class'], random_state=33)
+  x_val, x_test = x_val.reset_index(drop=True), x_test.reset_index(drop=True)
+  classes_val, classes_test =  classes_val.reset_index(drop=True), classes_test.reset_index(drop=True)
 
-rf_model = rf.RandomForest(42, x_train, y_train)
-f1_rf = rf.get_metrics(rf_model, x_val, y_val)
+  y_val, y_test = classes_val.apply(lambda c: 0 if c == 'normal' else 1), classes_test.apply(lambda c: 0 if c == 'normal' else 1)
 
-print(f1_rf)
+  del df_train, df_val_test  
+  
+  #print(x_train)
+  #NORMALIZANDO DADOS
+  #train
+  std_scaler = StandardScaler()
+  colunas_numericas = x_train.select_dtypes(include=['number'])
+  colunas_numericas_scaler = pd.DataFrame(std_scaler.fit_transform(colunas_numericas), columns=colunas_numericas.columns)
+  x_train = colunas_numericas_scaler
+  #print(x_train)
+  #val
+  colunas_numericas = x_val.select_dtypes(include=['number'])
+  colunas_numericas_scaler = pd.DataFrame(std_scaler.fit_transform(colunas_numericas), columns=colunas_numericas.columns)
+  x_val = colunas_numericas_scaler
+  #test
+  colunas_numericas = x_test.select_dtypes(include=['number'])
+  colunas_numericas_scaler = pd.DataFrame(std_scaler.fit_transform(colunas_numericas), columns=colunas_numericas.columns)
+  x_test = colunas_numericas_scaler
+  
+  return x_train, y_train, x_val, y_val
 
 #PROXIMAS AÇÕES: TESTAR O DATASET NOS ALGORITMOS, TESTAR A JUNÇÃO COM O PSO
--- pso2.py
+++ pso2.py
@@ -1,4 +1,5 @@
 from sklearn import preprocessing
+import dataset_supervised as ds
 preprocessing.LabelEncoder()
 import random
 import numpy as np
@@ -6,10 +7,6 @@ import warnings
 warnings.filterwarnings("ignore")
 import pandas as pd
 pd.set_option("display.max_columns", None)
-
-def accuracy_calc(x):
-    
-    return
 
 columnsName1=[0,1]
 particles=[]
@@ -20,27 +17,28 @@ for i in range(10):
         part1.append(item)
     particles.append(part1)
     
+    
 def data(particles1):
     particles2=[]
     for i in range(len(particles1)):
         if particles1[i]!=1:
-                particles2.append(columnsName[i])
+                particles2.append(ds.columnsName[i])
     return particles2
 
 pb=[]
-def checkpersonalnest():
+def checkpersonalnest(particles):
     for i in range(len(particles)):
-         pb.append(accuracy_calc(data(particles[i])))
+         pb.append(ds.accuracy_calc(data(particles[i])))
 checkpersonalnest()
 
-def checkvelocity(globalbest):
+def checkvelocity(globalbest, particles):
     velocity=[]
     for j in range(len(particles)):
         velocity.append(list(0+1*(np.random.random(1)[0])*(np.array(particles[j])-np.array(particles[j]))+1*(np.random.random(1)[0])*(np.array(globalbest)-np.array(particles[j]))))
     #print(velocity)
     return velocity
 
-def addingparticles(velocity):
+def addingparticles(velocity, particles):
     particles2=[]
     for i in range(len(velocity)):
         nextchromo=[]
@@ -58,10 +56,10 @@ def normalize(particles2):
                 particles2[l][m]=0
     return particles2
 
-def checkpd(particles2):
+def checkpd(particles2, particles):
     personal=[]
     for i in range(len(particles2)):
-        personal.append(accuracy_calc(data(particles2[i])))
+        personal.append(ds.accuracy_calc(data(particles2[i])))
     for j in range(len(personal)):
         if(personal[j]>pb[j]):
             particles[j]=particles2[j]
@@ -74,10 +72,11 @@ globalbest=particles[ind]
 for i in range(10):
     particles2=[]
     personal=[]
-    velocity=checkvelocity(globalbest)
-    particles2=addingparticles(velocity)
+    velocity=checkvelocity(globalbest, particles)
+    particles2=addingparticles(velocity, particles)
     particles2=normalize(particles2)
-    personal=checkpd(particles2)
+    personal=checkpd(particles2, particles)
+    particles = particles2
     globalbest=[]
     max(pb)
     ind = pb.index(max(pb))
@@ -90,3 +89,5 @@ ind = pb.index(max(pb))
 globalbest=particles[ind]
 
 print(data(globalbest))
+
+#FALTA CHECAR A ATUALIZAÇÃO DE VALORES DE PARTICLES
