82aad49a33b55318c8602f7b4322d173dd2ac674
João Vítor Régis <jvcr@cin.ufpe.br>
2024-02-07 14:38:38 -0300

    Remoção de valores NA do dataset UNSW-NB15

--- UNSW-NB15_dataset.py
+++ UNSW-NB15_dataset.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 # Features do dataset
 features = pd.read_csv(r'csv_NUSW-NB15\NUSW-NB15_features.csv', encoding='ISO-8859-1')
@@ -7,11 +9,12 @@ print(features)
 # Mapeamento dos tipos de dados
 type_mapping = {
     'integer': 'int',
+    'Integer': 'int',
     'Float': 'float',
-    'nominal': 'object',
-    'binary': 'bool',  # Ou 'object' se representar strings de bits
-    'Binary': 'bool',  # Ou 'object' se representar strings de bits
-    'Timestamp': 'datetime64'
+    'nominal': 'str',
+    'binary': 'int',  # Ou 'object' se representar strings de bits
+    'Binary': 'int',  # Ou 'object' se representar strings de bits
+    'Timestamp': 'float'
 }
 
 # Criando um dicionário para usar com 'dtype' em 'read_csv'
@@ -26,3 +29,14 @@ for number in range(1, 5):
     print(len(df_aux.columns))
     df_list.append(df_aux)
 df = pd.concat(df_list, ignore_index=True)
+
+NAcolumns = df.columns[df.isna().any(axis=0)]
+# ['ct_flw_http_mthd', 'is_ftp_login', 'attack_cat'] são colunas com valores NA
+# No caso de 'attack_cat' pode-se dizer que é o tipo do ataque. Quando ocorre NA significa que não foi ataque.
+# Dito isso, essa coluna não pode ser usada dito que ela literalmente diz a categoria do ataque.
+
+df = df.drop('attack_cat', axis=1)
+df['ct_flw_http_mthd'] = df['ct_flw_http_mthd'].fillna(-1) # Só ocorre se foi requisição http
+df['is_ftp_login'] = df['is_ftp_login'].fillna(-1) # Só ocorre se foi requisição ftp
+
+# Não há colunas com valores infinitos.
