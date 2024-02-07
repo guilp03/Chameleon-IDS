e7b7587bd6246385deeabc318403dfb5d7e66568
João Vítor Régis <jvcr@cin.ufpe.br>
2024-02-07 14:52:11 -0300

    Divisão e normalização do dataset UNSW-NB15

--- UNSW-NB15_dataset.py
+++ UNSW-NB15_dataset.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def normalize_data(subset):
+    std_scaler = StandardScaler()
+    colunas_numericas = subset.select_dtypes(include=['number'])
+    colunas_numericas_scaler = pd.DataFrame(std_scaler.fit_transform(colunas_numericas), columns=colunas_numericas.columns)
+    subset = colunas_numericas_scaler
+    return subset
 
 # Features do dataset
 features = pd.read_csv(r'csv_NUSW-NB15\NUSW-NB15_features.csv', encoding='ISO-8859-1')
@@ -49,3 +58,30 @@ for column in not_numeric_columns:
     # Garantindo que todos os valores da coluna sejam do tipo string
     df[column] = df[column].astype(str)
     df[column] = encoder.fit_transform(df[column])
+
+# Separando labels
+columnsName = df.drop(labels= 'Label', axis= 1).columns.values.tolist()
+y = df['Label']
+y = y.apply(lambda c: 0 if c == 'normal' else 1)
+
+# Divisão do conjunto de treino validação e teste
+# Dividindo a database em % para treinamento e % para validacao e testes
+x_train, x_val_test, y_train, y_val_test =  train_test_split(df[columnsName], y, test_size=0.3, random_state=42, stratify=df['Label'])
+
+# Reset dos índices dos subsets
+x_train = x_train.reset_index(drop=True)
+x_val_test = x_val_test.reset_index(drop=True)
+
+# Dividindo o subset de validação + teste em subset de validação e subset de testes
+x_val, x_test, y_val, y_test = train_test_split(x_val_test, y_val_test, test_size=0.65, stratify=y_val_test, random_state=33)
+  
+# Reset dos índices dos subsets
+x_val, x_test = x_val.reset_index(drop=True), x_test.reset_index(drop=True)
+y_val, y_test =  y_val.reset_index(drop=True), y_test.reset_index(drop=True)
+
+del x_val_test
+
+# Normalizando dados
+x_train = normalize_data(x_train)
+x_val = normalize_data(x_val)
+x_test = normalize_data(x_test)
