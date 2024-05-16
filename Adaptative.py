3dd4e253572d89fc0db9fc3c64192e10dc84ee44
guilp03 <108680176+guilp03@users.noreply.github.com>
2024-05-16 16:38:42 -0300

    Definições da adaptação de parâmetros

--- Adaptative.py
+++ Adaptative.py
@@ -1,8 +1,11 @@
 import math
 def euclid_distance(N, D, swarm, particle):
-    sum_d = 0
+    """ Retorna a distância euclidiana de uma partícula a todas as outras partículas  
+    """
+    sum_qrt_d = 0
     for i in range(N):
         particle_aux = swarm[i]
+        sum_d = 0
         if swarm[i] != particle:
             for j in range(D):
                 sum_d += (particle_aux.position[j] - particle.position[j])**2
@@ -19,45 +22,89 @@ def evo_factor(dg, dmin, dmax):
     return f
 
 def inertia_function(f):
+    """ Retorna o valor atual da inércia
+    """
     inertia = 1/ 1+1.5*math.e**(-2.6 * f)
     
     return inertia
 
-def f_value(f, case):
-    if case == 0:
+def membership(f, state):
+    """ Retorna o grau de pertencimento do evolutionary factor f ao estado passado como input
+    """
+    if state == "s1":
         if 0 <= f <= 0.4:
-            f = 0
+            membership = 0
         elif 0.4 < f <= 0.6:
-            f = 5 * f - 2
+            membership = 5 * f - 2
         elif 0.6 < f <= 0.7:
-            f = 1
+            membership = 1
         elif 0.7 < f <= 0.8:
-            f =-10 * f + 8
+            membership =-10 * f + 8
         elif 0.8 < f <= 1:
-            f = 0
-    elif case == 1:
+            membership = 0
+    elif state == "s2":
         if 0 <= f <= 0.2:
-            f = 0
+            membership = 0
         elif 0.2 < f <= 0.3:
-            f = 10 * f - 2
+            membership = 10 * f - 2
         elif 0.3 < f <= 0.4:
-            f = 1
+            membership = 1
         elif 0.4 < f <= 0.6:
-            f = -5 * f + 3
+            membership = -5 * f + 3
         elif 0.6 < f <= 1:
-            f = 0
-    elif case == 2:
+            membership = 0
+    elif state == "s3":
         if 0 <= f <= 0.1:
-            f = 1
+            membership = 1
         elif 0.1 < f <= 0.3:
-            f = -5 * f + 1.5
+            membership = -5 * f + 1.5
         elif 0.3 < f <= 1:
-            f = 0
-    elif case == 3:
+            membership = 0
+    elif state == "s4":
         if 0 <= f <= 0.7:
-            f = 0
+            membership = 0
         elif 0.7 < f <= 0.9:
-            f = 5 * f - 3.5
+            membership = 5 * f - 3.5
         elif 0.9 < f <= 1:
-            f = 1
-    return f
+            membership = 1
+    return membership
+
+def defuzzification(current_state, f):
+    """ Tomada de decisões de troca de estados a partir do evolutionary factor e o estado atual"""
+    membership = float
+    if 0 <= f <= 0.2:
+        new_state = "s3"
+    elif 0.3 < f <= 0.4:
+        new_state = "s2"
+    elif 0.6 < f <= 0.7:
+        new_state = "s1"
+    elif 0.8 < f <= 1:
+        new_state = "s4"
+    elif 0.2 < f <= 0.3:
+        if current_state == "s1" or current_state == "s2":
+            new_state = "s2"
+        elif current_state == "s3" or current_state == "s4":
+            if membership(f, "s2") > membership(f, "s3"):
+                new_state = "s2"
+            else:
+                new_state = "s3"
+    elif 0.4 < f <= 0.6:
+        if current_state == "s4" or current_state == "s1":
+            new_state = "s1"
+        elif current_state == "s2" or current_state == "s3":
+            if membership(f, "s1") > membership(f, "s2"):
+                new_state = "s1"
+            else:
+                new_state = "s2"
+    elif 0.7 < f <= 0.8:
+        if current_state == "s3" or current_state == "s4":
+            new_state = "s4"
+        elif current_state == "s2" or current_state == "s1":
+            if membership(f, "s4") > membership(f, "s1"):
+                new_state = "s4"
+            else:
+                new_state = "s1"
+    return new_state
+    
+    
+    
